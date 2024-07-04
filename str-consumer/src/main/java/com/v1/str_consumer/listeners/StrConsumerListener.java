65eb5608a199dc5e2807f1d5b710fa34f4493ddc
Carlos Soares <carlos.soares@zallpy.com>
2024-07-04 14:37:23 -0300

    strConsumer created

---
+++ str-consumer/src/main/java/com/v1/str_consumer/listeners/StrConsumerListener.java
@@ -0,0 +1,26 @@
+package com.v1.str_consumer.listeners;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+@Log4j2
+@Component
+public class StrConsumerListener {
+
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void created(String message){
+        log.info("CREATED ::: Receive message {}", message);
+
+    }
+
+    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void log(String message){
+        log.info("LOG ::: Receive message {}", message);
+
+    }
+    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    public void history(String message){
+        log.info("HISTORY ::: Receive message {}", message);
+
+    }
+}
