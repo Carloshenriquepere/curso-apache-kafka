9034104f92d60a815aac0ab19c43e669940edaff
Carlos Soares <carlos.soares@zallpy.com>
2024-07-04 15:00:15 -0300

    custom listener

--- str-consumer/src/main/java/com/v1/str_consumer/listeners/StrConsumerListener.java
+++ str-consumer/src/main/java/com/v1/str_consumer/listeners/StrConsumerListener.java
@@ -1,24 +1,24 @@
 package com.v1.str_consumer.listeners;
 
+import com.v1.str_consumer.custom.StrConsumerCustomListener;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 @Log4j2
 @Component
 public class StrConsumerListener {
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void created(String message){
         log.info("CREATED ::: Receive message {}", message);
 
     }
 
-    @KafkaListener(groupId = "group-1", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-1")
     public void log(String message){
         log.info("LOG ::: Receive message {}", message);
 
     }
-    @KafkaListener(groupId = "group-2", topics = "str-topic", containerFactory = "strContainerFactory")
+    @StrConsumerCustomListener(groupId = "group-2")
     public void history(String message){
         log.info("HISTORY ::: Receive message {}", message);
 
